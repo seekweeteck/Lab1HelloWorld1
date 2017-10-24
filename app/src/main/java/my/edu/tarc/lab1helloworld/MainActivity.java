218ba7068b371423a96660178fa97cb1bc28db5a
seekt <seekt@acd.tarc.edu.my>
2017-10-24 12:30:26 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/lab1helloworld/MainActivity.java
@@ -0,0 +1,37 @@
+package my.edu.tarc.lab1helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    //Global or module level variable
+    private EditText editTextName, editTextAge;
+    private TextView textViewMessage;
+
+    //onCreate = main() function
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Display the UI
+        //R = resources class
+        setContentView(R.layout.activity_main);
+
+        //Linking UI to program
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextAge = (EditText) findViewById(R.id.editTextAge);
+        textViewMessage = (TextView) findViewById(R.id.textViewMessage);
+    }
+    
+    public void displayMessage(View view){
+        String name;
+        int age;
+        name = editTextName.getText().toString();
+        age = Integer.parseInt(editTextAge.getText().toString());
+        age++;
+        textViewMessage.setText("Hi " + name + "");
+    }
+}
